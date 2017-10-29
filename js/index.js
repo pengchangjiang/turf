6586317570565aedda019729f4628ed467ef1e4d
pengcj <pcjgis@163.com>
2017-10-29 18:25:37 +0800

    完成初始框架搭建，along方法测试

---
+++ js/index.js
@@ -0,0 +1,80 @@
+var map, createType = 1;//0,1,2:point,line,polygon
+var pointArr = [], layerGroup;
+function init() {
+    map = L.map('map', { center: [36.77, 111.13], zoom: 4, doubleClickZoom: false });
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    layerGroup = L.layerGroup().addTo(map);
+    initMapEvent();
+}
+init();
+/**
+ * 清除
+ */
+function clearTest() {
+    layerGroup.clearLayers();
+    pointArr = [];
+}
+/**
+ * 创建点
+ */
+function changeCursor(grab) {
+    var mapC = L.DomUtil.get('map');
+    if (grab) {
+        L.DomUtil.addClass(mapC, 'leaflet-grab');
+    } else {
+        L.DomUtil.removeClass(mapC, 'leaflet-grab');
+    }
+}
+function create(type) {
+    changeCursor(false);
+    createType = type;
+}
+function finishCreate() {
+    changeCursor(true);
+}
+/**
+ * 激活地图事件
+ */
+function initMapEvent() {
+
+    map.on('click', function (e) {
+        var latlng = e.latlng;
+        switch (createType) {
+            case 0:
+                layerGroup.addLayer(L.marker([latlng.lat, latlng.lng]));
+                break;
+            case 1:
+            case 2:
+                pointArr.push([latlng.lng, latlng.lat]);
+                break;
+        }
+    });
+    map.on('dblclick', function (e) {
+        var latlngs = pointArr.map(function (lnglat) {
+            return [lnglat[1], lnglat[0]];
+        });
+
+        var geoLayer = null;
+        switch (createType) {
+            case 1:
+                geoLayer = L.polyline(latlngs, { color: '#674' });
+                break;
+            case 2:
+                geoLayer = L.polygon(latlngs, { color: '#674' });
+                break;
+        }
+        layerGroup.addLayer(geoLayer);
+    })
+}
+/**
+ * along 示例
+ */
+function alongTest() {
+    // var lnglats = [[116.45, 39.77], [112.41, 37.57], [110.91, 34.88], [112.93, 27.76]];
+    var line = turf.lineString(pointArr);
+    var along = turf.along(line, 200, 'miles');
+    var alongMarker = L.marker(along.geometry.coordinates.reverse());
+    layerGroup.addLayer(alongMarker);
+}
